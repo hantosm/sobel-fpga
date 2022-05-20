9a1bab989da8d48a25fa975da23c6871afb467ca
Marton Hantos <marton.hantos@pcbdesign.hu>
2022-05-21 00:13:55 +0200

    Added drawio diagrams

--- sobel_detection.tcl
+++ sobel_detection.tcl
@@ -18,6 +18,16 @@
 proc checkRequiredFiles { origin_dir} {
   set status true
   set files [list \
+ "[file normalize "$origin_dir/vivado_project/sobel_detection.srcs/utils_1/imports/synth_1/hdmi_top.dcp"]"\
+  ]
+  foreach ifile $files {
+    if { ![file isfile $ifile] } {
+      puts " Could not find local file $ifile "
+      set status false
+    }
+  }
+
+  set files [list \
  "[file normalize "$origin_dir/sources/hdmi_tx.edn"]"\
  "[file normalize "$origin_dir/sources/hdmi_rx.edn"]"\
  "[file normalize "$origin_dir/sources/dp_ram.v"]"\
@@ -29,10 +39,10 @@ proc checkRequiredFiles { origin_dir} {
  "[file normalize "$origin_dir/sources/sobel_wrapper.v"]"\
  "[file normalize "$origin_dir/sources/hdmi_top.v"]"\
  "[file normalize "$origin_dir/constraints/hdmi_top.xdc"]"\
- "[file normalize "$origin_dir/testbench/edge_detector_tb.v"]"\
  "[file normalize "$origin_dir/sources/line_buffer.v"]"\
  "[file normalize "$origin_dir/testbench/line_buffer_tb.v"]"\
  "[file normalize "$origin_dir/testbench/line_delay_tb.v"]"\
+ "[file normalize "$origin_dir/testbench/edge_detector_tb.v"]"\
   ]
   foreach ifile $files {
     if { ![file isfile $ifile] } {
@@ -129,6 +139,7 @@ set proj_dir [get_property directory [current_project]]
 
 # Set project properties
 set obj [current_project]
+set_property -name "board_part_repo_paths" -value "C:/Users/hanto/AppData/Roaming/Xilinx/Vivado/2021.2/xhub/board_store/xilinx_board_store" -objects $obj
 set_property -name "default_lib" -value "xil_defaultlib" -objects $obj
 set_property -name "enable_vhdl_2008" -value "1" -objects $obj
 set_property -name "ip_cache_permissions" -value "read write" -objects $obj
@@ -139,7 +150,7 @@ set_property -name "revised_directory_structure" -value "1" -objects $obj
 set_property -name "sim.central_dir" -value "$proj_dir/${_xil_proj_name_}.ip_user_files" -objects $obj
 set_property -name "sim.ip.auto_export_scripts" -value "1" -objects $obj
 set_property -name "simulator_language" -value "Mixed" -objects $obj
-set_property -name "webtalk.xsim_launch_sim" -value "78" -objects $obj
+set_property -name "webtalk.xsim_launch_sim" -value "191" -objects $obj
 
 # Create 'sources_1' fileset (if not found)
 if {[string equal [get_filesets -quiet sources_1] ""]} {
@@ -180,6 +191,7 @@ set_property -name "file_type" -value "EDIF" -objects $file_obj
 # Set 'sources_1' fileset properties
 set obj [get_filesets sources_1]
 set_property -name "top" -value "hdmi_top" -objects $obj
+set_property -name "top_auto_set" -value "0" -objects $obj
 
 # Create 'constrs_1' fileset (if not found)
 if {[string equal [get_filesets -quiet constrs_1] ""]} {
@@ -209,10 +221,10 @@ if {[string equal [get_filesets -quiet sim_1] ""]} {
 # Set 'sim_1' fileset object
 set obj [get_filesets sim_1]
 set files [list \
- [file normalize "${origin_dir}/testbench/edge_detector_tb.v"] \
  [file normalize "${origin_dir}/sources/line_buffer.v"] \
  [file normalize "${origin_dir}/testbench/line_buffer_tb.v"] \
  [file normalize "${origin_dir}/testbench/line_delay_tb.v"] \
+ [file normalize "${origin_dir}/testbench/edge_detector_tb.v"] \
 ]
 add_files -norecurse -fileset $obj $files
 
@@ -224,13 +236,26 @@ add_files -norecurse -fileset $obj $files
 
 # Set 'sim_1' fileset properties
 set obj [get_filesets sim_1]
-set_property -name "top" -value "edge_detector_tb" -objects $obj
+set_property -name "top" -value "line_buffer_tb" -objects $obj
 set_property -name "top_auto_set" -value "0" -objects $obj
 set_property -name "top_lib" -value "xil_defaultlib" -objects $obj
 
 # Set 'utils_1' fileset object
 set obj [get_filesets utils_1]
-# Empty (no sources present)
+# Add local files from the original project (-no_copy_sources specified)
+set files [list \
+ [file normalize "${origin_dir}/vivado_project/sobel_detection.srcs/utils_1/imports/synth_1/hdmi_top.dcp" ]\
+]
+set added_files [add_files -fileset utils_1 $files]
+
+# Set 'utils_1' fileset file properties for remote files
+# None
+
+# Set 'utils_1' fileset file properties for local files
+set file "synth_1/hdmi_top.dcp"
+set file_obj [get_files -of_objects [get_filesets utils_1] [list "*$file"]]
+set_property -name "netlist_only" -value "0" -objects $file_obj
+
 
 # Set 'utils_1' fileset properties
 set obj [get_filesets utils_1]
@@ -262,6 +287,7 @@ if { $obj != "" } {
 }
 set obj [get_runs synth_1]
 set_property -name "part" -value "xc7k70tfbg676-1" -objects $obj
+set_property -name "incremental_checkpoint" -value "$proj_dir/sobel_detection.srcs/utils_1/imports/synth_1/hdmi_top.dcp" -objects $obj
 set_property -name "auto_incremental_checkpoint" -value "1" -objects $obj
 set_property -name "strategy" -value "Vivado Synthesis Defaults" -objects $obj
 
